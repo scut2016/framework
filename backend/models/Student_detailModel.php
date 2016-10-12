0dee9f4fd9d40f1b3823e2ef771c06e0e300436e
liu <3046587188@qq.com>
2016-10-12 13:40:38 +0800

    Model

---
+++ backend/models/Student_detailModel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 文件名：StudentDetail.php
+ * 文件说明:
+ * 时间: 2016/10/12.13:38
+ */
+
+namespace backend\models;
+
+
+use vendor\base\Model;
+
+class Student_detailModel extends Model
+{
+
+}
